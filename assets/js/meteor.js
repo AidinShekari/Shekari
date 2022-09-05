78bd4d87061fbc45705de0e856455c391fd7cbe8
Aidin Shekari <59771519+DarkZo0m@users.noreply.github.com>
2022-09-05 15:30:20 +0430

    Update meteor.js

--- assets/js/meteor.js
+++ assets/js/meteor.js
@@ -33,19 +33,6 @@ function createMeteors() {
 
 setInterval(createMeteors, 6000);
 
-$(document).on('touchstart', function(e) {
-    if (e.target.nodeName !== 'INPUT') {
-        e.preventDefault();
-    }
-});
-
-// prevent scrolling from within input field
-$(document).on('touchmove', function(e) {
-    if (e.target.nodeName == 'INPUT') {
-        e.preventDefault();
-    }
-});
-
 function createStars() {
 	let i;
 	let noOfStars = window.innerWidth < 700 ? 15 : 50;
